87a64b63b68b86e4c37b7d462831fd60fe5c9a99
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-09-19 10:02:53 -0400

    removed old logging funcs

--- mlogging.h
+++ mlogging.h
@@ -1,7 +1,7 @@
 #ifndef MTECH__PRETTY_TERMINAL_COLORS_H_
     #define MTECH__PRETTY_TERMINAL_COLORS_H_
 
-    /* Terminal Colors */
+    /** Terminal Colors */
     
     /* Reset */
     #define SET_DEFAULT "\033[0m"
@@ -25,54 +25,8 @@
     #define BG_WHITE  "\033[47m"
 
     /* Logging Functions */
-    #include <time.h>
-    #define CURRENT_TIME_STR_LEN 25
-
-    static inline const char* _getCurrentTime(void) {
-        time_t currentTime;
-        time(&currentTime);
-        char* strTime = ctime(&currentTime);
-        /* Removes the trailing '\n' character */
-        strTime[CURRENT_TIME_STR_LEN - 2] = '\0';
-        return strTime;
-    }
-
 
     #include <stdio.h>
-    #include <stdarg.h>
-    #include <stdbool.h>
-    typedef _Bool boolean_t;
-    /* TIME | [TYPE] | INFO | PATH */
-
-    static inline void _mlog_logNormal(const char* msg, const char* path) {
-        printf("%24s | [NORMAL] | %s | %s \n", _getCurrentTime(), msg, path);
-    }
-
-
-    static inline void _mlog_logFormattedInfo(const char* path, const char* msg, ...) {
-        va_list ap;
-        va_start(ap, msg);
-        printf("%24s | [NORMAL] | ", _getCurrentTime());
-        vprintf(msg, ap);
-        printf(" | %s \n", path);
-        va_end(ap);
-    }
-
-
-    static inline void _mlog_logError(const char* msg, const char* path) {
-        fprintf(stderr, "%24s | %s[ERROR]%s | %s | %s \n",\
-            _getCurrentTime(), FG_RED, SET_DEFAULT, msg, path);
-    }
-
-    static inline void _mlog_logFormattedError(const char* path, const char* msg, ...) {
-        va_list ap;
-        va_start(ap, msg);
-        printf("%24s | %s[ERROR]%s | ", _getCurrentTime(), FG_RED, SET_DEFAULT);
-        vfprintf(stderr, msg, ap);
-        printf(" | %s \n", path);
-        va_end(ap);
-    }
-
 
     static inline void MLOG_run_tests(const char* tag, int cond) {
         if (cond) {
@@ -81,11 +35,5 @@
             printf("%s: %s[FAIL]%s\n", tag, FG_RED, SET_DEFAULT);
         }
     }
-
-
-    #define MLOG_logNormal(msg) _mlog_logNormal(msg, __FILE__)
-    #define MLOG_logFormattedInfo(msg, ...) _mlog_logFormattedInfo(__FILE__, msg, __VA_ARGS__)
-    #define MLOG_logError(msg) _mlog_logError(msg, __FILE__)
-    #define MLOG_logFormattedError(msg, ...) _mlog_logFormattedError(__FILE__, msg, __VA_ARGS__)
 
 #endif

471f0b625f3cdc4d37d3c88e4476beb29724622d
BlueHatDude <ehmdiagne0530@gmail.com>
2024-10-31 21:13:47 -0400

    added testing functions

--- mlogging.h
+++ mlogging.h
@@ -2,35 +2,35 @@
     #define MLOGGING_LIBRARY_H_
 
     /** Terminal Colors */
-    
+
     /* Reset */
     #define SET_DEFAULT "\033[0m"
-    /* Foreground Colors */
+    /* Terminal Colors */
     #define NO_COLOR SET_DEFAULT
-    #define COLOR_BLACK  "\033[0;30m"
-    #define COLOR_RED    "\033[0;31m"
-    #define COLOR_GREEN  "\033[0;32m"
-    #define COLOR_YELLOW "\033[0;33m"
-    #define COLOR_BLUE   "\033[0;34m"
-    #define COLOR_PURPLE "\033[0;35m"
-    #define COLOR_CYAN   "\033[0;36m"
-    #define COLOR_WHITE  "\033[0;37m"
+    #define TC_BLACK  "\033[0;30m"
+    #define TC_RED    "\033[0;31m"
+    #define TC_GREEN  "\033[0;32m"
+    #define TC_YELLOW "\033[0;33m"
+    #define TC_BLUE   "\033[0;34m"
+    #define TC_PURPLE "\033[0;35m"
+    #define TC_CYAN   "\033[0;36m"
+    #define TC_WHITE  "\033[0;37m"
 
     /* Logging Functions */
 
     #include <stdio.h>
     #include <stdarg.h>
-    
+
     /* basic logging */
 
-    // ! WARNING: this function should not be called directly. Instead, use corresponding macro 
-    // * Newline is included as part of function. 
+    // ! WARNING: this function should not be called directly. Instead, use corresponding macro
+    // * Newline is included as part of function.
     static void mlog_log(const char* msg) {
         printf("[LOG]: %s\n", msg);
     }
 
     // ! WARNING: this function should not be called directly. Instead, use corresponding macro.
-    // * Newline is included as part of function. 
+    // * Newline is included as part of function.
     static void mlog_logf(const char* msg, ...) {
         va_list args;
         va_start(args, msg);
@@ -41,13 +41,13 @@
     }
 
     // ! WARNING: this function should not be called directly. Instead, use corresponding macro.
-    // * Newline is included as part of function. 
+    // * Newline is included as part of function.
     static void mlog_logc(const char* color, const char* msg) {
         printf("%s[LOG]: %s%s\n", color, msg, NO_COLOR);
     }
 
     // ! WARNING: this function should not be called directly. Instead, use corresponding macro.
-    // * Newline is included as part of function. 
+    // * Newline is included as part of function.
     static void mlog_logfc(const char* color, const char* msg, ...) {
         va_list args;
         va_start(args, msg);
@@ -60,13 +60,13 @@
 
     /* error logging */
     // ! WARNING: this function should not be called directly. Instead, use corresponding macro.
-    // * Newline is included as part of function. 
+    // * Newline is included as part of function.
     static void mlog_error(const char* msg) {
         fprintf(stderr, "[ERROR]: %s\n", msg);
     }
 
     // ! WARNING: this function should not be called directly. Instead, use corresponding macro.
-    // * Newline is included as part of function. 
+    // * Newline is included as part of function.
     static void mlog_errorf(const char* msg, ...) {
         va_list args;
         va_start(args, msg);
@@ -77,13 +77,13 @@
     }
 
     // ! WARNING: this function should not be called directly. Instead, use corresponding macro.
-    // * Newline is included as part of function. 
+    // * Newline is included as part of function.
     static void mlog_errorc(const char* color, const char* msg) {
         fprintf(stderr, "%s[ERROR]: %s%s\n", color, msg, NO_COLOR);
     }
 
     // ! WARNING: this function should not be called directly. Instead, use corresponding macro.
-    // * Newline is included as part of function. 
+    // * Newline is included as part of function.
     static void mlog_errorfc(const char* color, const char* msg, ...) {
         va_list args;
         va_start(args, msg);
@@ -95,49 +95,91 @@
 
 
     /* test logging */
-    // static void mlog_test(int cond, const char* msg);
-    // static void mlog_testf(int cond, const char* msg, ...);
-    // static void mlog_testc(int cond, const char* color, const char* msg);
-    // static void mlog_testfc(int cond, const char* color, const char* msg, ...);
+    static void mlog_test(int cond, const char* tag) {
+        if (cond) {
+            printf("[TEST] %s: %s[PASSED]%s\n", tag, TC_GREEN, NO_COLOR);
+        } else {
+            printf("[TEST] %s: %s[FAILED]%s\n", tag, TC_RED, NO_COLOR);
+        }
+    }
+
+    static void mlog_testf(int cond, const char* tag, ...) {
+        va_list args;
+        va_start(args, tag);
+        printf("[TEST] ");
+        vprintf(tag, args);
+
+        if (cond) {
+            puts(": " TC_GREEN "[PASSED]" NO_COLOR);
+        } else {
+            puts(": " TC_RED "[FAILED]" NO_COLOR);
+        }
+
+        va_end(args);
+    }
+
+    static void mlog_testc(const char* color, int cond, const char* tag) {
+        if (cond) {
+            printf("%s[TEST] %s: %s[PASSED]%s\n", color, tag, TC_GREEN, NO_COLOR);
+        } else {
+            printf("%s[TEST] %s: %s[FAILED]%s\n", color, tag, TC_RED, NO_COLOR);
+        }
+    }
+
+    static void mlog_testfc(const char* color, int cond, const char* tag, ...) {
+        va_list args;
+        va_start(args, tag);
+        printf("%s[TEST] ", color);
+        vprintf(tag, args);
+
+        if (cond) {
+            puts(": " TC_GREEN "[PASSED]" NO_COLOR);
+        } else {
+            puts(": " TC_RED "[FAILED]" NO_COLOR);
+        }
+
+        va_end(args);
+    }
+
 
 
     #ifndef MLOGOFF
         /* standard */
-        #define MLOG_log(msg) mlog_log(msg); 
+        #define MLOG_log(msg) mlog_log(msg);
         #define MLOG_error(msg) mlog_error(msg);
-        #define MLOG_test(cond, msg) ;
+        #define MLOG_test(cond, tag) mlog_test(cond, tag);
 
         /* format */
-        #define MLOG_logf(msg, ...) mlog_logf(msg, __VA_ARGS__); 
+        #define MLOG_logf(msg, ...) mlog_logf(msg, __VA_ARGS__);
         #define MLOG_errorf(msg, ...) mlog_errorf(msg, __VA_ARGS__);
-        #define MLOG_testf(cond, msg) ;
+        #define MLOG_testf(cond, tag, ...) mlog_testf(cond, tag, __VA_ARGS__);
 
         /* color */
-        #define MLOG_logc(color, msg) mlog_logc(color, msg); 
+        #define MLOG_logc(color, msg) mlog_logc(color, msg);
         #define MLOG_errorc(color, msg) mlog_errorc(color, msg);
-        #define MLOG_testc(color, cond, msg) ;
+        #define MLOG_testc(color, cond, tag) mlog_testc(color, cond, tag);
 
         /* color and format */
-        #define MLOG_logfc(color, msg, ...) mlog_logfc(color, msg, __VA_ARGS__); 
+        #define MLOG_logfc(color, msg, ...) mlog_logfc(color, msg, __VA_ARGS__);
         #define MLOG_errorfc(color, msg, ...) mlog_errorfc(color, msg, __VA_ARGS__);
-        #define MLOG_testfc(color, cond, msg, ...) ;
+        #define MLOG_testfc(color, cond, tag, ...) mlog_testfc(color, cond, tag, __VA_ARGS__);
     #endif
 
     #ifdef MLOGOFF
-        #define MLOG_log(msg) ; 
-        #define MLOG_error(msg) ;
-        #define MLOG_test(cond, msg) ;
+        #define MLOG_log(msg) ((void) 0);
+        #define MLOG_error(msg) ((void) 0);
+        #define MLOG_test(cond, msg) ((void) 0);
 
-        #define MLOG_logf(msg, ...) ; 
-        #define MLOG_errorf(msg, ...) ;
-        #define MLOG_testf(cond, msg) ;
+        #define MLOG_logf(msg, ...) ((void) 0);
+        #define MLOG_errorf(msg, ...) ((void) 0);
+        #define MLOG_testf(cond, msg) ((void) 0);
 
-        #define MLOG_logc(color, msg) ; 
-        #define MLOG_errorc(color, msg) ;
-        #define MLOG_testc(color, cond, msg) ;
+        #define MLOG_logc(color, msg) ((void) 0);
+        #define MLOG_errorc(color, msg) ((void) 0);
+        #define MLOG_testc(color, cond, msg) ((void) 0);
 
-        #define MLOG_logfc(color, msg, ...) ; 
-        #define MLOG_errorfc(color, msg, ...) ;
-        #define MLOG_testfc(color, cond, msg, ...) ;
+        #define MLOG_logfc(color, msg, ...) ((void) 0);
+        #define MLOG_errorfc(color, msg, ...) ((void) 0);
+        #define MLOG_testfc(color, cond, msg, ...) ((void) 0);
     #endif
 #endif
--- tests.c
+++ tests.c
@@ -5,15 +5,18 @@ int main(void) {
     /* normal logging functions */
     MLOG_log("Hello, World");
     MLOG_logf("My name is %s", "John Smith");
-    MLOG_logc(COLOR_BLUE, "Lorem ipsum");
-    MLOG_logfc(COLOR_RED, "%s %s", "Merry", "Christmas");
+    MLOG_logc(TC_BLUE, "Lorem ipsum");
+    MLOG_logfc(TC_RED, "%s %s", "Merry", "Christmas");
 
     /* error logging functions */
     MLOG_error("An error occured");
     MLOG_errorf("An %s occured | Code: %d", "error", 404);
-    MLOG_errorc(COLOR_BLUE, "Another error occured");
-    MLOG_errorfc(COLOR_RED, "This error occured %s", "mistakes were made");
+    MLOG_errorc(TC_BLUE, "Another error occured");
+    MLOG_errorfc(TC_RED, "This error occured %s", "mistakes were made");
 
     /* testing functions */
-
+    MLOG_test(5 == 5, "Obvious");
+    MLOG_testf(5 == 10, "Still %s", "obvious");
+    MLOG_testc(TC_BLUE, (1 == 1), "checking if math still works");
+    MLOG_testfc(TC_RED, (4 == 2.0), "Funny number %d", 420);
 }
